ae6209fedeba23e66387d3fa93aeb47c22721882
Łukasz Sypniewski <u589242@gdnmac035.ent.off.lsy.pl>
2018-02-14 15:51:41 +0100

    Created data structures, function for making REST call, tableView prototype.

---
+++ Task/ArticleHeadingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleHeadingTableViewCell.swift
+//  Task
+//
+//  Created by Łukasz Sypniewski on 14/02/2018.
+//  Copyright © 2018 Łukasz Sypniewski. All rights reserved.
+//
+
+import UIKit
+
+class ArticleHeadingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Task/ArticlesHeadingTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  ArticlesHeadingTableViewCell.swift
+//  Task
+//
+//  Created by Łukasz Sypniewski on 14/02/2018.
+//  Copyright © 2018 Łukasz Sypniewski. All rights reserved.
+//
+
+import UIKit
+
+class ArticleHeadingTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var articleHeadingImage: UIImageView!
+    @IBOutlet weak var articleHeadingTitle: UILabel!
+    @IBOutlet weak var articleHeadingSource: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
