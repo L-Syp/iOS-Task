0560f2693924966cc3032a5f65e0bd7a256f7e0f
Łukasz Sypniewski <u589242@gdnmac035.ent.off.lsy.pl>
2018-02-15 14:20:15 +0100

    Implemented image caching in UITableView.

--- Task/ArticleHeadingTableViewController.swift
+++ Task/ArticleHeadingTableViewController.swift
@@ -9,12 +9,9 @@
 import UIKit
 
 class ArticleHeadingTableViewController: UITableViewController {
-
-
-    
-
     let apiKey = "2beb5953fd92424983abae1dc1c7d58c"
     var articles = [Article]()
+    var cachedImages = [UIImage?]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,25 +20,24 @@ class ArticleHeadingTableViewController: UITableViewController {
         //self.tableView.reloadData()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return articles.count
     }
-
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleHeadingCell", for: indexPath) as? ArticleHeadingTableViewCell else {
@@ -56,72 +52,82 @@ class ArticleHeadingTableViewController: UITableViewController {
         
         cell.articleHeadingTitle.text = articles[indexPath.row].title ?? "No title"
         cell.articleHeadingSource.text = "Source: \(articles[indexPath.row].sourceName ?? "-")"
-        cell.articleHeadingImage.image = #imageLiteral(resourceName: "newsImage")
+        cell.articleHeadingImage.image = #imageLiteral(resourceName: "newsImage") //without this line some images are duplicated
+        
         if let imgURL = articles[indexPath.row].urlToImage {
-            let session = URLSession.shared
-            let task = session.dataTask(with: imgURL) { (data, response, error) in
-                if error == nil {
-                    let downloadedImage = UIImage(data: data!)
-                    DispatchQueue.main.async {
-                        cell.articleHeadingImage.image = downloadedImage
+            if let cachedImage = cachedImages[indexPath.row] {
+                cell.articleHeadingImage.image = cachedImage
+                print("************** Set an image cell from cache")
+            } else {
+                let session = URLSession.shared
+                let task = session.dataTask(with: imgURL) { (data, response, error) in
+                    if error == nil {
+                        let downloadedImage = UIImage(data: data!)
+                        print("************** Downloaded an image cell")
+                        self.cachedImages[indexPath.row] = downloadedImage
+                        DispatchQueue.main.async {
+                            cell.articleHeadingImage.image = downloadedImage
+                            print("************** Set an image cell from web")
+                        }
                     }
                 }
+                task.resume()
             }
-            task.resume()
         }
         return cell
     }
- 
-
+    
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
     func loadData() {
         RestCall.makeGetCall(endpoint: RestCall.Endpoints.topHeadlines, itemsCount: 30, additionalQueries: [URLQueryItem(name: "country", value: "us")], apiKey: apiKey) { dane in
             for i in 0..<dane.articles.count {
                 self.articles.append(Article(with: dane.articles[i]))
+                self.cachedImages.append(nil)
             }
             DispatchQueue.main.async {
                 self.tableView.reloadData()
@@ -129,5 +135,5 @@ class ArticleHeadingTableViewController: UITableViewController {
         }
     }
     
-
+    
 }
