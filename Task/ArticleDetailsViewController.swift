769fc0c47d594374fd9c65febe0ff9b2c7f80d3f
Łukasz Sypniewski <u589242@gdnmac035.ent.off.lsy.pl>
2018-02-21 16:12:46 +0100

    Fixed issues with displaying images in details view.

--- Task/ArticleDetailsViewController.swift
+++ Task/ArticleDetailsViewController.swift
@@ -10,25 +10,25 @@ import UIKit
 
 class ArticleDetailsViewController: UIViewController {
 
+    // MARK: Outlets
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var pubishedAtLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var authorLabel: UILabel!
     @IBOutlet weak var URLLabel: UILabel!
+    @IBOutlet var rootView: UIView!
     
+    // MARK: Properties
     var article: Article? = nil
     var image: UIImage? = nil
     var defaultImage: UIImage? = nil
-    @IBOutlet weak var containerView: UIView!
     
+    
+    // MARK: Outlets
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageView.image = image
-        if image == defaultImage {
-            imageView.contentMode = .scaleAspectFit
-        }
-        resizeView()
+        setViewImage()
         titleLabel.text = article!.title
         pubishedAtLabel.text = "Published at: \(article!.publishedAt ?? "-")"
         descriptionLabel.text = article!.description ?? "No description available"
@@ -36,24 +36,51 @@ class ArticleDetailsViewController: UIViewController {
         URLLabel.text = "URL: \(article!.url?.absoluteString ?? "No url available")"
     }
 
+    // MARK: Methods
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    func resizeView() {
-        let containerView = self.containerView!
-        
-        if let Aimage = image {
-            let ratio = Aimage.size.width / Aimage.size.height
-            if containerView.frame.width > containerView.frame.height {
-                let newHeight = containerView.frame.width / ratio
-                imageView.frame.size = CGSize(width: containerView.frame.width, height: newHeight)
-            }
-            else{
-                let newWidth = containerView.frame.height * ratio
-                imageView.frame.size = CGSize(width: newWidth, height: containerView.frame.height)
-            }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(deviceRotated), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+    }
+    
+    @objc func deviceRotated(){
+        setViewImage()
+    }
+    
+    // MARK: Image setting functions
+    fileprivate func setViewImage() {
+        imageView.image = resizeImage(image!, targetSize: rootView.frame.size) ?? defaultImage!
+        if image == defaultImage {
+            imageView.contentMode = .scaleAspectFit
         }
+    }
+    
+    func resizeImage(_ image: UIImage, targetSize: CGSize) -> UIImage? {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / image.size.width
+        let heightRatio = targetSize.height / image.size.height
+        
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
+        }
+        
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
     }
 }
 
